6d7ed6bc1ea15ce2fa16b350f5ea967f058eb07b
duorfan <nf2111@nyu.edu>
2021-11-23 00:54:42 -0500

    Top Stories

--- nyt api.js
+++ nyt api.js
@@ -1,14 +1,31 @@
-let endpoint = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=yourkey
-`;
+//https://api.nytimes.com/svc/topstories/v2/world.json?api-key=yourkey
+//n7AjUVNVn8wsg7q5RmfLwn3aeJMoAVdG
 
-let imageElement = document.getElementById("dogImg");
+let url = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=n7AjUVNVn8wsg7q5RmfLwn3aeJMoAVdG";
 
-fetch(endpoint)
+let headlines = document.getElementById("headlines");
 
-	.then(function(response){
-		return response.json();
-	})
+fetch(url).then(response => response.json())
+		  .then(data => {
+			console.log(data);
+			
+			data.results.map(article => {
 
-	.then(function(jsonData){
-		imageElement.src = jsonData.message;
-	})
+				console.log(article.title);
+
+				let a = document.createElement("a");
+				a.setAttribute('href', article.url);
+				a.innerHTML = article.title;
+
+				let b = document.createElement("b");
+				b.innerHTML = article.abstract;
+
+				let img = document.createElement("img");
+				img.setAttribute('src', article.multimedia[0].url);
+
+				headlines.appendChild(a);
+				headlines.appendChild(b);	
+				headlines.append(img);		
+
+			})
+		})
